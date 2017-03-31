9bece9b0d4f6927ca7d8cdd2f6c778a31ce855a4
Daulet Zhalpakov <daulet.zhalpakov@red-gate.com>
2017-03-31 15:58:06 +0100

    Someone forgot to commit stuff....

--- Exercise1/PrimeNumbersGenerator.cs
+++ Exercise1/PrimeNumbersGenerator.cs
@@ -1,6 +1,23 @@
-﻿namespace Exercise1
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise1
 {
     public class PrimeNumbersGenerator
     {
+        public IEnumerable<int> Generate(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (n > 2)
+            {
+                return new[] {2};
+            }
+
+            return new int[] {};
+        }
     }
 }
--- Exercise1_Tests/PrimeNumbersGeneratorTests.cs
+++ Exercise1_Tests/PrimeNumbersGeneratorTests.cs
@@ -1,9 +1,35 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Exercise1;
+using NUnit.Framework;
 
 namespace Exercise1_Tests
 {
     [TestFixture]
     public class PrimeNumbersGeneratorTests
     {
+        [Test]
+        public void GivenZero_ReturnsEmptyResult()
+        {
+            Assert.That(GenerateNumbers(0), Is.EquivalentTo(new int[] {}));
+        }
+
+        [Test]
+        public void GivenNegative_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => GenerateNumbers(-1));
+        }
+
+        [Test]
+        [TestCase(3, new [] { 2 })]
+        public void GivenUpperBound_ReturnsCorrectPrimes(int n, IEnumerable<int> primes)
+        {
+            Assert.That(GenerateNumbers(n), Is.EquivalentTo(primes));
+        }
+
+        private static IEnumerable<int> GenerateNumbers(int n)
+        {
+            return new PrimeNumbersGenerator().Generate(n);
+        }
     }
 }
